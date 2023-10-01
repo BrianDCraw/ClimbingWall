6fb520343f89e7298a69ee331e1322001ee8ad5a
BrianDCraw <briandcraw@gmail.com>
2023-09-30 18:06:50 -0700

    updates to handel OTA updates

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <FS.h>
 #include <Wire.h>
 #include <Adafruit_NeoPixel.h>
+#include <ArduinoOTA.h>
 
 WiFiMulti wifiMulti;
 const uint32_t connectTimeoutMs = 10000;
@@ -225,6 +226,9 @@ void setup_routing() {
   server.begin();
 }
 
+  
+
+
 void setup() 
 {
   SPIFFS.begin(true); //intialize file system
@@ -239,7 +243,40 @@ void setup()
   pixels.begin();
   pixels.clear();
   pixels.show();
+  
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
+
  
 void loop() 
 {
@@ -248,5 +285,6 @@ void loop()
   if (WiFi.status() != WL_CONNECTED) {
        connectToWiFi();
   }
+    ArduinoOTA.handle();
 }
 
